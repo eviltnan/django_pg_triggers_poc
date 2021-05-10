80644aeffa99cf7b91f15b56ae0a8cd8dc93c24c
Thorin Schiffer <thorin@schiffer.pro>
2021-05-10 14:23:37 +0200

    Plan of checking

--- Readme.md
+++ Readme.md
@@ -4,3 +4,14 @@
 
 - python 3.9.x
 - binaries (postgres python3 extension) -> install_binaries.sh
+
+## Comments
+
+- not sure which python version is in the extension
+
+## Steps
+- install simple python function over migration from code
+- trigger (https://django-pgtrigger.readthedocs.io/en/latest/tutorial.html#keeping-a-field-in-sync-with-another)
+- install function with TD
+- load project within interpreter
+- access ORM within function
--- triggers/tests.py
+++ triggers/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.db import connection
+from pytest import fixture
 
-# Create your tests here.
+from triggers.models import Book
+
+
+@fixture
+def book(db):
+    return Book.objects.create(name='book')
+
+
+@fixture
+def simple_function():
+    with open('pl_python/simple_function.sql', 'r') as f:
+        return f.read()
+
+
+def test_simple_function(simple_function, db):
+    with connection.cursor() as cursor:
+        cursor.execute(simple_function)
+        cursor.execute('select pymax(10, 20)')
+        row = cursor.fetchone()
+    assert row[0] == 20
