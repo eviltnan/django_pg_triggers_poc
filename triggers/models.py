91ea3e4a7ec8a1ec5ca200b00704dceafc795fde
Thorin Schiffer <thorin@schiffer.pro>
2021-05-11 10:52:04 +0200

    Added amount fields

--- triggers/models.py
+++ triggers/models.py
@@ -1,5 +1,7 @@
-from django.db.models import Model, CharField
+from django.db.models import Model, CharField, IntegerField
 
 
 class Book(Model):
     name = CharField(max_length=10)
+    amount_stock = IntegerField(default=20)
+    amount_sold = IntegerField(default=10)
