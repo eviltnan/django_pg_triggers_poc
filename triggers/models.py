df7fc8298a100fb643d2421641fd7fcd4346e6f8
Thorin Schiffer <thorin@schiffer.pro>
2021-07-01 12:41:03 +0200

    Added some usage example to the book model

--- triggers/models.py
+++ triggers/models.py
@@ -1,4 +1,4 @@
-from django.db.models import Model, CharField, IntegerField
+from django.db.models import Model, CharField, IntegerField, Func, F
 
 from triggers.pl_python.builder import plfunction
 
@@ -15,3 +15,8 @@ class Book(Model):
     name = CharField(max_length=10)
     amount_stock = IntegerField(default=20)
     amount_sold = IntegerField(default=10)
+
+    def get_max(self):
+        return Book.objects.annotate(
+            max_value=Func(F('amount_sold'), F('amount_stock'), function='pymax')
+        ).get(pk=self.pk).max_value
