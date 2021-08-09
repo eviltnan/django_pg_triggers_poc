4ac211f72e5da33c5356bcd09626a315f7464b54
Thorin Schiffer <thorin@schiffer.pro>
2021-08-09 16:47:48 +0200

    Some spelling fixes

--- triggers/management/commands/syncfunctions.py
+++ triggers/management/commands/syncfunctions.py
@@ -5,21 +5,21 @@ from triggers.pl_python.builder import pl_functions, install_function, pl_trigge
 
 
 class Command(BaseCommand):
-    help = 'Syncs PL/Python functions, decorated with @plfunction'
+    help = 'Syncs PL/Python functions, decorated with @plfunction and @pltrigger'
 
     @transaction.atomic
     def handle(self, *args, **options):
         if not pl_functions and not pl_triggers:
             self.stdout.write("No PL/Python functions found")
 
-        self.stdout.write("Synching functions")
+        self.stdout.write("Syncing functions")
         for function_name, f in pl_functions.items():
-            self.stdout.write(f"Synching {function_name}")
+            self.stdout.write(f"Syncing {function_name}")
             install_function(f)
             self.stdout.write(f"Installed {function_name}")
 
-        self.stdout.write("Synching triggers")
+        self.stdout.write("Syncing triggers")
         for function_name, f in pl_triggers.items():
-            self.stdout.write(f"Synching {function_name}")
+            self.stdout.write(f"Syncing {function_name}")
             install_function(f[0], f[1])
             self.stdout.write(f"Installed {function_name}")
